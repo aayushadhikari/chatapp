1c50b18e37d0b7a2bb1e1cb457b019ab82ece879
Aayush Adhikari <aayushadhikari11@gmail.com>
2020-06-10 19:02:50 -0400

    fix

--- src/layouts/SignedIn.js
+++ src/layouts/SignedIn.js
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 
-const SignedIn = () => {
-  return <h1>Bhai</h1>;
+let socket;
+const SignedIn = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const endpoint = "localhost:3005";
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+    socket = io(endpoint);
+    setName(name);
+    setRoom(room);
+    socket.emit("join", { name: name, room: room }, () => {});
+    return () => {
+      socket.emit("disconnect");
+      socket.off();
+    };
+  }, [endpoint, location.search]);
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    if (message) {
+      socket.emit("sendMessage", message, () => {
+        setMessage("");
+      });
+    }
+  };
+
+  console.log(message, messages);
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <input
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+          onKeyPress={(event) =>
+            event.key === "Enter" ? sendMessage(event) : null
+          }
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SignedIn;
