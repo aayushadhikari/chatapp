0d38f95965d8b756bccf497fd9d2420e595cd48e
Aayush Adhikari <aayushadhikari11@gmail.com>
2020-06-10 19:44:10 -0400

    dayend

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactEmoji from "react-emoji";
 
 const Message = ({ message: { user, text }, name }) => {
   let isSentByCurrentUser = false;
@@ -12,14 +13,14 @@ const Message = ({ message: { user, text }, name }) => {
     <div className="messageContainer justifyEnd">
       <p className="sentText">{trimmedName}</p>
       <div className="messageBox backgroundBlue">
-        <p className="messageText colorWhite">{text}</p>
+        <p className="messageText colorWhite">{ReactEmoji.emojify(text)}</p>
       </div>
     </div>
   ) : (
     <div className="messageContainer justifyStart">
       <p className="sentText">{trimmedName}</p>
       <div className="messageBox backgroundLight">
-        <p className="messageText colorBlack">{text}</p>
+        <p className="messageText colorBlack">{ReactEmoji.emojify(text)}</p>
       </div>
     </div>
   );
