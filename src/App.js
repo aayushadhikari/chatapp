654c1cd45b42a8adb96fa9851eae442a7fc9c0d7
Aayush Adhikari <aayushadhikari11@gmail.com>
2020-06-10 16:23:22 -0400

    layouts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import SignedOut from "./layouts/SignedOut";
+import SignedIn from "./layouts/SignedIn";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export default App = () => {
+  <Router>
+    <Route path="/" exact component={SignedOut} />
+    <Route path="/chat" exact component={SignedIn} />
+  </Router>;
+};
